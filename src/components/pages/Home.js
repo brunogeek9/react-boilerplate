98cc3fb1a9b0a52dad91e493202a4aff20995b4c
brunogeek9 <brunogeek9@gmail.com>
2020-04-29 19:52:27 -0300

    teste

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,6 +6,7 @@ class Home extends Component {
     return (
       <div>
         <h1>Welcome to the Jamelli React Boilerplate</h1>
+        <h2>Teste</h2>
         <h2>The time is: <Clock /> and counting! </h2>
       </div>
     )
